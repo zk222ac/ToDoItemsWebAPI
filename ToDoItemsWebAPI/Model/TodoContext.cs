310595046957702980c48a9ca7fea91bf2cfd7c1
Zuhair Haroon Khan <zk222ac@gmail.com>
2018-09-26 13:45:11 +0200

    Final release

---
+++ ToDoItemsWebAPI/Model/TodoContext.cs
@@ -0,0 +1,25 @@
+﻿        using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+        using Microsoft.EntityFrameworkCore;
+
+namespace ToDoItemsWebAPI.Model
+{
+    public class TodoContext : DbContext
+    {
+
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+
+        }
+
+        public TodoContext()
+        {
+            
+        }
+
+        public DbSet<ToDoItem> TodoItem { get; set; }
+    }
+}
